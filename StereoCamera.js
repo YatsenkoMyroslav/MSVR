1ba1ed98aeec3b1e908a55440daf7e57bb10ddda
Myroslav Yatsenko <mirik297@gmail.com>
2025-06-03 22:34:20 +0300

    PA-2 complete

--- StereoCamera.js
+++ StereoCamera.js
@@ -1,38 +1,46 @@
-class StereoCamera {
-    constructor(eyeSeparation, convergence, aspectRatio, fov, nearClip, farClip) {
-        this.eyeSeparation = eyeSeparation;
-        this.convergence = convergence;
-        this.aspectRatio = aspectRatio;
-        this.fov = fov;
-        this.nearClip = nearClip;
-        this.farClip = farClip;
+function StereoCamera(eyeSeparation,
+    convergence,
+    aspectRatio,
+    FOV,
+    nearClippingDistance,
+    farClippingDistance)
+{
+    this.eyeSeparation = eyeSeparation;
+    this.convergence = convergence;
+    this.mAspectRatio = aspectRatio;
+    this.FOV = FOV;
+    this.nearClippingDistance = nearClippingDistance;
+    this.farClippingDistance = farClippingDistance;  
+
+    this.calcLeftFrustum = function()
+    {
+        let top, bottom, left, right;
+        top = this.nearClippingDistance * Math.tan(this.FOV / 2);
+        bottom = -top;
+
+        let a = this.mAspectRatio * Math.tan(this.FOV / 2) * this.convergence;
+        let b = a - this.eyeSeparation / 2;
+        let c = a + this.eyeSeparation / 2;
+
+        left = -b * this.nearClippingDistance / this.convergence;
+        right = c * this.nearClippingDistance / this.convergence;
+
+        return m4.frustum(left, right, bottom, top, this.nearClippingDistance, this.farClippingDistance);
     }
 
-    calcLeftFrustum() {
-        const top = this.nearClip * Math.tan(this.fov / 2);
-        const bottom = -top;
-        
-        const a = this.aspectRatio * Math.tan(this.fov / 2) * this.convergence;
-        const b = a - this.eyeSeparation / 2;
-        const c = a + this.eyeSeparation / 2;
-        
-        const left = -b * this.nearClip / this.convergence;
-        const right = c * this.nearClip / this.convergence;
-        
-        return m4.frustum(left, right, bottom, top, this.nearClip, this.farClip);
-    }
+    this.calcRightFrustum = function()
+    {
+        let top, bottom, left, right;
+        top = this.nearClippingDistance * Math.tan(this.FOV / 2);
+        bottom = -top;
 
-    calcRightFrustum() {
-        const top = this.nearClip * Math.tan(this.fov / 2);
-        const bottom = -top;
-        
-        const a = this.aspectRatio * Math.tan(this.fov / 2) * this.convergence;
-        const b = a - this.eyeSeparation / 2;
-        const c = a + this.eyeSeparation / 2;
-        
-        const left = -c * this.nearClip / this.convergence;
-        const right = b * this.nearClip / this.convergence;
-        
-        return m4.frustum(left, right, bottom, top, this.nearClip, this.farClip);
+        let a = this.mAspectRatio * Math.tan(this.FOV / 2) * this.convergence;
+        let b = a - this.eyeSeparation / 2;
+        let c = a + this.eyeSeparation / 2;
+
+        left = -c * this.nearClippingDistance / this.convergence;
+        right = b * this.nearClippingDistance / this.convergence;
+
+        return m4.frustum(left, right, bottom, top, this.nearClippingDistance, this.farClippingDistance);
     }
-} 
+}
